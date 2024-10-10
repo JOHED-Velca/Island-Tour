cd60791c8d60dd535a88c0972b33d897fe960cca
Jonathan Velasquez <johedvelca@gmail.com>
2024-10-10 13:42:27 -0400

    v3

--- islands-main.c
+++ islands-main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
-typedef struct iland
+typedef struct island
 {
     char* name;
     char* opens;
     char* closes;
     struct  island* next;
 }island;
+
+void display(island* start)
+{
+    island* i = start;
+    for (;i!= NULL;i= i->next)
+    {
+        //printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
+        printf("Name: %s \n", i->name);
+    }
+}
+
+void menu()
+{
+    int option;
+
+    printf("--------------ISLANDS TOUR--------------\n");
+    printf("\t1. Show current itinerary.");
+    printf("\t2. Change itinerary.");
+    printf("\t3. Exit");
+    printf("----------------------------------------\n");
+    printf("Choose an option (1-3): ");
+    scanf("%d", option);
+}
 
 int main(int argc, char* argv[]) {
     island amity = {
@@ -40,6 +63,22 @@ int main(int argc, char* argv[]) {
     amity.next = &craggy;
     craggy.next = &IslaNublar;
     IslaNublar.next = &shutter;
+
+    display(&amity);
+    
+    printf("\n");
+
+    island skull = {
+        .name = "Skull",
+        .opens = "09:00",
+        .closes = "17:00",
+        .next = NULL
+    };
+
+    IslaNublar.next = &skull;
+    skull.next = &shutter;
+
+    display(&amity);
 
     return 0;
 }
